daf3c7b581aa03c1311159f3c8e7f6bed1544bf9
Riccardo Lazzari <153769506+Riccalaz@users.noreply.github.com>
2024-10-03 19:55:36 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,23 @@
-package unpunc11;
+package unpunc;
 
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-		Scanner myObj = new Scanner(System.in);
-		buffer bu=new buffer();
-		
-		produttore p=new produttore(bu);
-		consumatore c=new consumatore(bu);
-		p.start();
-		c.start();
-		
-		
-	}
+    public static void main(String[] args) throws InterruptedException {
+	    
+        Scanner myObj = new Scanner(System.in);
 
+        // Creo un buffer per produttore e consumatore
+        buffer bu = new buffer();  
+
+        // Creo il thread per il produttore
+        produttore p = new produttore(bu);
+
+        // Creo il thread per il consumatore
+        consumatore c = new consumatore(bu);
+
+        p.start();
+	c.start();
+    }
 }
